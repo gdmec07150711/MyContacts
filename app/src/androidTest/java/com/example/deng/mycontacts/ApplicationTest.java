533d52c3dadf73beccd62286729632664ad8e9db
gdmec07150711 <418390144@qq.com>
2017-01-02 14:55:38 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/deng/mycontacts/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.example.deng.mycontacts;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
